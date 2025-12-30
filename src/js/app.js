01b54ee2d62f85c6dc06f8e052e2e16c49fa0c61
Daniel César <144414027+Dasksz@users.noreply.github.com>
2025-12-30 15:02:39 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: window.location.origin
+                redirectTo: window.location.origin + window.location.pathname
             }
         });
 
